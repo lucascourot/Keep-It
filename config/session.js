87d7a9a07e65fafa14102362647f3bab823b4b6a
Lucas Courot <lucas@courot.com>
2013-12-06 05:25:03 +0100

    Add Redis for session management

--- config/session.js
+++ config/session.js
@@ -15,23 +15,23 @@ module.exports.session = {
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again. 
-  secret: '647c25877ca6ed2ee5ca8c20917804f3'
+  secret: '647c25877ca6ed2ee5ca8c20917804f3',
 
 
-  // In production, uncomment the following lines to set up a shared redis session store
-  // that can be shared across multiple Sails.js servers
-  // adapter: 'redis',
-  //
-  // The following values are optional, if no options are set a redis instance running
-  // on localhost is expected.
-  // Read more about options at: https://github.com/visionmedia/connect-redis
-  //
-  // host: 'localhost',
-  // port: 6379,
-  // ttl: <redis session TTL in seconds>,
-  // db: 0,
-  // pass: <redis auth password>
-  // prefix: 'sess:'
+//  In production, uncomment the following lines to set up a shared redis session store
+//  that can be shared across multiple Sails.js servers
+  adapter: 'redis',
+
+//  The following values are optional, if no options are set a redis instance running
+//  on localhost is expected.
+//  Read more about options at: https://github.com/visionmedia/connect-redis
+
+  host: 'localhost',
+  port: 6379,
+  ttl: 10000,
+  db: 0,
+  pass: null,
+  prefix: 'sess:'
 
 
   // Uncomment the following lines to use your Mongo adapter as a session store
